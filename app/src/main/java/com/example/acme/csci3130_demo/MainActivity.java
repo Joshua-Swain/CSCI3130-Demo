0f188c0138b3f2b304b9d5aa0fd59dd81b16b1bd
Joshua Swain <Joshua.Swain@dal.ca>
2018-05-16 20:35:48 -0300

    Added text input, and button to replace center text with text from new input

--- app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
+++ app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
@@ -2,6 +2,13 @@ package com.example.acme.csci3130_demo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final Button gobble = findViewById(R.id.replace_text_button);
+        gobble.setOnClickListener(new View.OnClickListener() {
+            public void onClick (View v) {
+                EditText textInput = findViewById(R.id.text_input);
+                Editable newText = textInput.getText();
+                TextView mainText = findViewById(R.id.main_text);
+                mainText.setText(newText);
+            }
+        });
     }
 }
